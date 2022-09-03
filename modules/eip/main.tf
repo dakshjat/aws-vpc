c1ddef3ae16036eef43035b7fc9cb5681af35bf6
Daksh Jat <dakshjt70@gmail.com>
2022-09-03 16:54:51 +0530

    Updated eip module with vpc argument

--- modules/eip/main.tf
+++ modules/eip/main.tf
@@ -1,3 +1,4 @@
 resource "aws_eip" "this" {
   tags   = var.tags
+  vpc    = var.vpc
 }
--- modules/eip/variables.tf
+++ modules/eip/variables.tf
@@ -2,3 +2,9 @@ variable "tags" {
   type        = map
   description = "A map of tags to assign to the resource"  
 }
+
+variable "vpc" {
+  type        = bool
+  default     = true
+  description = "The EIP is in a VPC or not"  
+}
